eed8fdff0e4556e1afe61ae845cd3424d3efafcf
yarasrepo <yara.m.tassi@gmail.com>
2024-06-10 13:43:44 +0300

    6-13

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     './public/**/*.{html,js}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ['Satoshi']
+      }
+    },
   },
   plugins: [],
 }
